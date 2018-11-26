aab8a420c0d54c981d32b0a0773bb65fe6fdd079
Susan Meyers <smmeyers24@gmail.com>
2018-11-26 20:14:12 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,29 @@
+require 'pry'
 class Board
   @@all = []
   attr_accessor :cells
   
-  def reset!
-    cells.clear
+  def initialize
+    @cells =  Array.new(9, " ")
+    
+  end
   
+  def reset!
+    @cells.clear
+    @cells = Array.new(9, " ")
+  end
+  
+  def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+  
+  def position(input)
+      @cells
+      
     
   end
   
